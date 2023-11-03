4f0ccef2e4d76ae8e68a95ae2fe49d8871922a20
xiyuee3 <3560367449@qq.com>
2023-11-03 20:56:17 +0800

    Create mpu6050.h
    添加mpu6050.h文件

---
+++ mpu6050.h
@@ -0,0 +1,23 @@
+/*
+ * mpu6050.h
+ *
+ *  Created on: Nov 3, 2023
+ *      Author: Asus
+ */
+
+#ifndef INC_MPU6050_H_
+#define INC_MPU6050_H_
+
+typedef struct
+{
+	float acc_angle[2];   //加速度计数据得到姿态角1
+	float d_angle[3];     //陀螺仪数据换算出的角速度
+	float groy_angle[3];  //陀螺仪数据得出的姿态角2
+	float angle[3];       //互补姿态角 依次存入roll,pitch,yaw
+}mpu6050;
+
+
+void mpu_update(mpu6050 *mpudata,float acc[3],float groy[3],float K,float dt);
+void mpu_angle(mpu6050 *mpudata,float angles[3]);
+
+#endif /* INC_MPU6050_H_ */
